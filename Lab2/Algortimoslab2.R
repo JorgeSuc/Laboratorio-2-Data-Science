f1f9fdcb755f37ad7662d158f99ecab1e9531159
JorgeSuc <jorgesm595@gmail.com>
2018-08-28 16:53:58 -0600

    Add files via upload

--- Lab2/Algortimoslab2.R
+++ Lab2/Algortimoslab2.R
@@ -36,8 +36,7 @@ trainSet<-datause[thanos,] #Obtengo las filas de los elementos que estan en el s
 testestSet<-datause[-thanos,] #Obtengo las filas de los elementos que no están en el vector de muestra
 
  
-View(trainSet)
-View(testestSet)
+
 
 ###################################################
 
@@ -56,7 +55,7 @@ View(testestSet)
 modelineal<-lm(SalePrice~., data = trainSet)
 summary(modelineal)
 
-View(testestSet)
+
 
 #Calculamos la prediccion
 prediccion<-predict(modelineal,newdata = testestSet[1:36])
@@ -65,5 +64,38 @@ dif<-abs(testestSet$PricePred-testestSet$SalePrice)
 
 testestSet$mpgPred <-NULL
 
+#########################################################
+
+######### acá viene la parte de decision del analista####
+
+View(trainSet)
+View(testestSet)
+
+
+############################################################################
+#### luego de obtener las correlaciones de nuestra data ahora obtengo
+#### las columnas que tengan mayor significancia para ver que pasa
+
+
+datito <- trainSet[,c("YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF",
+                      "LowQualFinSF","BsmtHalfBath","FullBath","HalfBath","GarageArea",         
+                      "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch", "SalePrice")]
+
+testdatito <- testestSet[,c("YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF",
+                            "LowQualFinSF","BsmtHalfBath","FullBath","HalfBath","GarageArea",         
+                            "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch","SalePrice")]
+
+###########################################################################
+### ahora he de hacer la prediccion pero ahora con las que tienen mayor peso
+
+
+modelodura<-lm(SalePrice~., data = datito)
+summary(modelodura)
+
+#Calculamos la prediccion
+prediccion<-predict(modelodura,newdata = testdatito[2:ncol(testdatito)])
+testdatito$PricePred<-prediccion
+difI<-abs(testdatito$PricePred-testdatito$SalePrice)
+ 
 
 
