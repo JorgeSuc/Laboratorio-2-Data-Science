552134dd15c12c8713442a2d1eb5bb94abda5a4d
JorgeSuc <jorgesm595@gmail.com>
2018-08-28 22:19:16 -0600

    Add files via upload

--- Lab2/Algortimoslab2.R
+++ Lab2/Algortimoslab2.R
@@ -61,6 +61,8 @@ summary(modelineal)
 prediccion<-predict(modelineal,newdata = testestSet[1:36])
 testestSet$PricePred<-prediccion
 dif<-abs(testestSet$PricePred-testestSet$SalePrice)
+View(dif
+     )
 
 testestSet$mpgPred <-NULL
 
@@ -77,25 +79,102 @@ View(testestSet)
 #### las columnas que tengan mayor significancia para ver que pasa
 
 
-datito <- trainSet[,c("YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF",
+datito <- trainSet[,c( "SalePrice","YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF",
                       "LowQualFinSF","BsmtHalfBath","FullBath","HalfBath","GarageArea",         
-                      "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch", "SalePrice")]
+                      "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch")]
 
-testdatito <- testestSet[,c("YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF",
+testdatito <- testestSet[,c("SalePrice","YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF",
                             "LowQualFinSF","BsmtHalfBath","FullBath","HalfBath","GarageArea",         
-                            "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch","SalePrice")]
+                            "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch")]
 
 ###########################################################################
 ### ahora he de hacer la prediccion pero ahora con las que tienen mayor peso
+
+
+
 
 
 modelodura<-lm(SalePrice~., data = datito)
 summary(modelodura)
 
 #Calculamos la prediccion
+# se toma las columnas desde la columna 2 (dejando afuera el precio por el momento)
 prediccion<-predict(modelodura,newdata = testdatito[2:ncol(testdatito)])
 testdatito$PricePred<-prediccion
-difI<-abs(testdatito$PricePred-testdatito$SalePrice)
+difdura<-abs(testdatito$PricePred-testdatito$SalePrice)
+
+##################################################################################################
+########################## KNN####################################################################
+
+
+
+
+# nota: Las categoricas no se les puede hacer la knn por lo mismo, se tratará de convertirla a numerica 
+
+# 1 - minimo
+# 2 - medio bajo
+# 3 - medio
+# 4 - alto
+# 5 brutal
+
+
+
+# valor mas grande 755000
+# valor mas pequenio 3400
+
+
+datito$Prec[is.na(datito$SalePrice)] = "1"
+datito$Prec[datito$SalePrice>=340000 & datito$SalePrice<180000] = "1"
+datito$Prec[datito$SalePrice>=180000 & datito$SalePrice<325000] = "2"
+datito$Prec[datito$SalePrice>=325000 & datito$SalePrice<470000] = "3"
+datito$Prec[datito$SalePrice>=470000 & datito$SalePrice<620000] = "4"
+datito$Prec[datito$SalePrice>=620000 & datito$SalePrice<=755000]= "5"
+
+
+testdatito$Prec[is.na(testdatito$SalePrice)] = "1"
+testdatito$Prec[testdatito$SalePrice>=340000 & testdatito$SalePrice<180000] = "1"
+testdatito$Prec[testdatito$SalePrice>=180000 & testdatito$SalePrice<325000] = "2"
+testdatito$Prec[testdatito$SalePrice>=325000 & testdatito$SalePrice<470000] = "3"
+testdatito$Prec[testdatito$SalePrice>=470000 & testdatito$SalePrice<620000] = "4"
+testdatito$Prec[testdatito$SalePrice>=620000 & testdatito$SalePrice<=755000]= "5"
+
+testdatito[is.na(testdatito)] <- 0
+datito[is.na(datito)] <- 0
+
+
+
+###################
+
+### prediccion knn raiz de toda la data da 24.16 entonces se utiliza la k de 23 
+# todos los NA en 0s
+
+predKnn<-knn(datito[,c(2:15)], testdatito[,c(2:15)], as.factor(datito$Prec),k=23)
+ 
+
+View(datito)
+View(testdatito)
+cfm<-confusionMatrix(as.factor(testdatito$Prec), predKnn)
+cfm
+
+
+
+#Con caret usando validacion cruzada
+set.seed(55)
+lrx8 <- trainControl(method = "repeatedcv",
+                       number = 10,
+                       repeats = 23)
+
+
+knnTrain <- train(Prec~., data = testdatito, method = "knn",
+                  trControl=trctrl,
+                  preProcess = c("center", "scale"), tuneLength=10)
+
+predknn<-predict(knnTrain,newdata = testdatito[,c(1:16)])
+summary(knnTrain)
+cfm<-confusionMatrix(as.factor(testdatito$Prec), predKnn)
+cfm
+
+
  
 
 
